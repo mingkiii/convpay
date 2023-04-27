b1bef13a352ee190a251b2dc61aca247f58be9c7
mingkiii <kimchunjae92@gmail.com>
2023-04-28 00:51:10 +0900

    할인 기능 개발 수정(OCP)

--- src/main/java/com/zerobase/convpay/service/ConveniencePayService.java
+++ src/main/java/com/zerobase/convpay/service/ConveniencePayService.java
@@ -9,6 +9,7 @@ import com.zerobase.convpay.type.*;
 public class ConveniencePayService {
     private final MoneyAdapter moneyAdapter = new MoneyAdapter();
     private final CardAdapter cardAdapter = new CardAdapter();
+    private final DiscountInterface discountInterface = new DiscountByPayMethod();
 
     public PayResponse pay(PayRequest payRequest) {
         PaymentInterface paymentInterface;
@@ -19,7 +20,9 @@ public class ConveniencePayService {
             paymentInterface = moneyAdapter;
         }
 
-        PaymentResult paymentResult = paymentInterface.payment(payRequest.getPayAmount());
+        Integer discountedAmount = discountInterface.getDiscountedAmount(payRequest);
+
+        PaymentResult paymentResult = paymentInterface.payment(discountedAmount);
         //Fail Case
         if (paymentResult == PaymentResult.PAYMENT_FAIL) {
             return new PayResponse(PayResult.FAIL, 0);
@@ -28,7 +31,7 @@ public class ConveniencePayService {
         //Exception Case...
 
         //Success Case
-        return new PayResponse(PayResult.SUCCESS, payRequest.getPayAmount());
+        return new PayResponse(PayResult.SUCCESS, discountedAmount);
     }
 
     public PayCancelResponse payCancel(PayCancelRequest payCancelRequest) {
--- src/test/java/com/zerobase/convpay/service/ConveniencePayServiceTest.java
+++ src/test/java/com/zerobase/convpay/service/ConveniencePayServiceTest.java
@@ -23,13 +23,13 @@ class ConveniencePayServiceTest {
         PayResponse payResponse = conveniencePayService.pay(payRequest);
         //then
         assertEquals(PayResult.SUCCESS, payResponse.getPayResult());
-        assertEquals(100, payResponse.getPaidAmount());
+        assertEquals(70, payResponse.getPaidAmount());
     }
 
     @Test
     void pay_fail() {
         //given
-        PayRequest payRequest = new PayRequest(PayMethodType.MONEY, ConvenienceType.G25, 1000_001);
+        PayRequest payRequest = new PayRequest(PayMethodType.MONEY, ConvenienceType.G25, 1500_001);
 
         //when
         PayResponse payResponse = conveniencePayService.pay(payRequest);
